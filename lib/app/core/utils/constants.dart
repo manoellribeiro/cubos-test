6b6d1d705173a791e31d4c9852967561f6938715
Manoel Ribeiro <mano12328@hotmail.com>
2020-09-23 15:47:02 -0300

    feat: create utils folder and constants file

---
+++ lib/app/core/utils/constants.dart
@@ -0,0 +1,6 @@
+const THE_MOVIE_DB_BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'cbc0416a39448ac090408129af32203f';
+const ACTION_GENRE_ID = 28;
+const ADVENTURE_GENRE_ID = 12;
+const COMEDY_GENRE_ID = 35;
+const FANTASY_GENRE_ID = 14;
