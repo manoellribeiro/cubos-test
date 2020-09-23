b873b5551385fbba40c9417fdb0632f761584af8
Manoel Ribeiro <mano12328@hotmail.com>
2020-09-23 17:50:37 -0300

    feat: create discover movies remote datasource implementation

--- lib/app/features/list_movies/data/datasources/discover_movies_remote_datasource/discover_movies_remote_datasource_implementation.dart
+++ lib/app/features/list_movies/data/datasources/discover_movies_remote_datasource/discover_movies_remote_datasource_implementation.dart
@@ -1,3 +1,5 @@
+import 'package:cubos_test/app/core/errors/exceptions/server_exception.dart';
+import 'package:cubos_test/app/core/utils/constants.dart';
 import 'package:cubos_test/app/features/list_movies/data/datasources/discover_movies_remote_datasource/discover_movies_remote_datasource.dart';
 import 'package:cubos_test/app/features/list_movies/domain/entities/DiscoverMoviesApiResponse.dart';
 import 'package:dio/dio.dart';
@@ -5,12 +7,27 @@ class DiscoverMoviesRemoteDataSourceImplementation implements DiscoverMoviesRemo
   
   final Dio dio;
 
-  DiscoverMoviesRemoteDataSourceImplementation(this.dio);
-  
-  
+  DiscoverMoviesRemoteDataSourceImplementation({this.dio});
+
   @override
-  Future<DiscoverMoviesApiResponse> getMoviesList(int genreId, int pageNumber) {
-    
+  Future<DiscoverMoviesApiResponse> getMoviesList(int genreId, int pageNumber) async {
+    try {
+      var response
+       = await dio.get(
+         'discover/movie',
+         queryParameters: {
+           "api_key": API_KEY,
+           "language": "pt-BR",
+           "sort_by": "popularity.desc",
+           "include_adult": false,
+           "include_video": false,
+           "page": pageNumber,
+           "with_genres": "$genreId"
+         }
+         );  
+    } catch (e) {
+      throw ServerException();
+    }
   }
 
 }
