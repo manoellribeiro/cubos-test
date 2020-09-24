6aa39f0c74e5cd88b6607dbffbb7eaaf4ca74cc5
Manoel Ribeiro <mano12328@hotmail.com>
2020-09-24 14:50:55 -0300

    feat: add id to MovieResults

--- lib/app/features/list_movies/domain/entities/MovieResults.dart
+++ lib/app/features/list_movies/domain/entities/MovieResults.dart
@@ -2,13 +2,15 @@ class MovieResults {
 	String posterPath;
 	List<int> genreIds;
 	String title;
+  int id;
 
-	MovieResults({this.posterPath, this.genreIds, this.title});
+	MovieResults({this.posterPath, this.genreIds, this.title, this.id});
 
 	MovieResults.fromJson(Map<dynamic, dynamic> json) {
 		posterPath = json['poster_path'];
 		genreIds = json['genre_ids'].cast<int>();
 		title = json['title'];
+    id= json['id'];
 	}
 
 	Map<String, dynamic> toJson() {
@@ -16,6 +18,7 @@ class MovieResults {
 		data['poster_path'] = this.posterPath;
 		data['genre_ids'] = this.genreIds;
 		data['title'] = this.title;
+    data['id'] = this.id;
 		return data;
 	}
 }
