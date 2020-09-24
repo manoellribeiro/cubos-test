7b60840448ebe3a4b3d0b91b92ad87218809a6eb
Manoel Ribeiro <mano12328@hotmail.com>
2020-09-24 16:26:52 -0300

    feat: create movie details page and movie details controller

---
+++ lib/app/features/show_movie_details/presentation/views/controller/movie_details_controller.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'movie_details_controller.g.dart';
+
+class MovieDetailsController = _MovieDetailsControllerBase with _$MovieDetailsController;
+
+abstract class _MovieDetailsControllerBase with Store {
+  
+}
---
+++ lib/app/features/show_movie_details/presentation/views/movie_details_page.dart
@@ -0,0 +1,17 @@
+import 'package:cubos_test/app/features/show_movie_details/presentation/views/controller/movie_details_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class MovieDetailsPage extends StatefulWidget {
+  @override
+  _MovieDetailsPageState createState() => _MovieDetailsPageState();
+}
+
+class _MovieDetailsPageState extends ModularState<MovieDetailsPage, MovieDetailsController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
