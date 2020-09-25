4d8cc7ea4d1949958ca44360b7a06f3e3bb23b4b
Manoel Ribeiro <mano12328@hotmail.com>
2020-09-25 14:31:50 -0300

    test: getGenreNameById function

---
+++ test/app/core/functions/getGenreNameById_test.dart
@@ -0,0 +1,10 @@
+import 'package:cubos_test/app/core/functions/getGenreNameById.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+
+  test('getGenreNameById is returning the correct value', (){
+    expect(getGenreNameById(16), "Animação");
+  });
+
+}
