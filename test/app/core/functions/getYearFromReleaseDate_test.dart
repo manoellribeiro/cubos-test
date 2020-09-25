d70ac073b5f3634c2b3b67eaf4acb31299fc2be1
Manoel Ribeiro <mano12328@hotmail.com>
2020-09-25 14:34:07 -0300

    test: getYearFromReleaseDate function

---
+++ test/app/core/functions/getYearFromReleaseDate_test.dart
@@ -0,0 +1,10 @@
+import 'package:cubos_test/app/core/functions/getYearFromReleaseDate.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+
+  test('getYearFromReleaseDate is returning the correct value', (){
+    expect(getYearFromReleaseDate("2020-07-07"), "2020");
+  });
+
+}
