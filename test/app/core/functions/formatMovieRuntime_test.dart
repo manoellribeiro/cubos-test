d966c11aba6fa95459a88b24b070e369a0e1258b
Manoel Ribeiro <mano12328@hotmail.com>
2020-09-25 14:28:59 -0300

    test: formatMovieRuntime function

---
+++ test/app/core/functions/formatMovieRuntime_test.dart
@@ -0,0 +1,11 @@
+import 'package:cubos_test/app/core/functions/formatMoneyValueToDollar.dart';
+import 'package:cubos_test/app/core/functions/formatMovieRuntime.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+
+  test('formatMovieRuntime is returning the correct value', (){
+    expect(formatMovieRuntime(100), "1h 40 min");
+  });
+
+}
